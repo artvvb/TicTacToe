b7e2b7ca62b4a26950e39058dd5e410dc32b1180
Arthur Brown <arthur.brown@email.wsu.edu>
2016-01-07 12:57:31 -0800

    Updated go_encode comments

--- sources/go_encode.v
+++ sources/go_encode.v
@@ -6,6 +6,7 @@ module go_encode(
     //gameover: bool win, bool winner, bool line[8]
     wire [15:0] line;
     
+    // P1 has won in one of eight ways: row 1-3, column 1-3, diagonal up left to bottom right, diagonal up right to bottom left
     assign line[ 0] = mem[ 0] & mem[ 2] & mem[ 4];
     assign line[ 1] = mem[ 8] & mem[10] & mem[12];
     assign line[ 2] = mem[16] & mem[18] & mem[20];
@@ -15,6 +16,7 @@ module go_encode(
     assign line[ 6] = mem[ 0] & mem[10] & mem[20];
     assign line[ 7] = mem[ 4] & mem[10] & mem[16];
     
+    // P2 has won (in the same ways/order as P1 above)
     assign line[ 8] = mem[ 1] & mem[ 3] & mem[ 5];
     assign line[ 9] = mem[ 9] & mem[11] & mem[13];
     assign line[10] = mem[17] & mem[19] & mem[21];
@@ -28,6 +30,7 @@ module go_encode(
     generate for (i=0; i<8; i=i+1)
         assign gameover[i] = line[i] | line[i+8];
     endgenerate
-    assign gameover[8] = gameover[9] & |line[15:8];
-    assign gameover[9] = |gameover[7:0];
+    assign gameover[8] = gameover[9] & |line[15:8]; // game is over and P2 has won
+    assign gameover[9] = |gameover[7:0]; // game is over
+    // [9:8] == 2'b10 implies that game is over and p1 has won
 endmodule
